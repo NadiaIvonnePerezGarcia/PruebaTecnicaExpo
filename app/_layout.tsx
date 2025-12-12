c1d8a5b4dfcef69cb23cd31ec4e2e0715b77e9c8
Nadia Ivonne Perez García <197950725+NadiaIvonnePerezGarcia@users.noreply.github.com>
2025-12-12 16:02:10 -0600

    nitial setup: Expo project structure, dependencies, and navigation config

---
+++ app/_layout.tsx
@@ -0,0 +1,21 @@
+import "../global.css";
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Inicio" }} />
+      <Stack.Screen name="detail" options={{ title: "Detalle" }} />
+    </Stack>
+  );
+}
